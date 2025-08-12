cea6029951d7632ba3349c6e12a1b60ebef99606
Marco Romeo <marco.romeo@omninext.it>
2025-08-12 11:43:40 +0200

    feat: add test

--- src/test/java/com/awesome/pizza/PizzaApplicationTests.java
+++ src/test/java/com/awesome/pizza/PizzaApplicationTests.java
@@ -1,13 +1,20 @@
 package com.awesome.pizza;
 
+import com.awesome.pizza.repository.OrderRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, properties = {
+		"aws.region=eu-central-1",
+		"spring.autoconfigure.exclude=org.springframework.cloud.function.serverless.web.ServerlessAutoConfiguration"
+})
 class PizzaApplicationTests {
+
+	@MockBean
+	OrderRepository orderRepository;
 
 	@Test
 	void contextLoads() {
 	}
-
 }
